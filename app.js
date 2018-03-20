6646b691ea45759e7693482e6947d93ac794e5d8
Aaron Moore <x2kicker2x@gmail.com>
2018-03-20 02:25:05 -0400

    Please work

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
+require('dotenv').config()
 
 const port = process.env.PORT || 8080
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-var http = require('http').Server(app);
-var bodyParser = require('body-parser');
+var http = require('http').Server(app)
+var bodyParser = require('body-parser')
 
 
-app.use(bodyParser.json());
-app.use('/', require('./routes/tweets'));
+app.use(bodyParser.json())
+app.use('/', require('./routes/tweets'))
 
 
 http.listen(port, () => {
-  console.log('server is up:', port);
-});
+  console.log('server is up:', port)
+})
--- routes/tweets.js
+++ routes/tweets.js
@@ -1,13 +1,13 @@
-var router = require('express').Router();
-var Twitter = require('twitter');
-var tweets = [];
+var router = require('express').Router()
+var Twitter = require('twitter')
+var tweets = []
 var keys = require('./config')
 var twitterAccount = new Twitter(keys.configKeys)
 
 
 router.use((req, res, next) => {
-  res.header('access-control-allow-origin', '*');
-  res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('access-control-allow-origin', '*')
+  res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next();
 })
 
@@ -19,14 +19,14 @@ router.post("/", (req,res) => {
     let length = tweet.statuses.length
 
     for (var x = 0; x < length; x++) {
-      tweets.push({tweet: tweet.statuses[x].text});
+      tweets.push({tweet: tweet.statuses[x].text})
     }
     res.send(tweets)
   })
   .catch((err) => {
-    console.log(err);
-    throw error;
+    console.log(err)
+    throw error
   });
 })
 
-module.exports = router;
+module.exports = router
