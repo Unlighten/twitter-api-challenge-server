3b896d2488ca1362c1e28021bc13b76b681a4908
Aaron Moore <x2kicker2x@gmail.com>
2018-03-20 10:04:08 -0400

    Finished around 2:30am... Realized things were broken around the morning time... still need to fix stuff and make this look way better

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,24 +1,28 @@
 var router = require('express').Router()
 var Twitter = require('twitter')
 var tweets = []
-var keys = require('./config')
-var twitterAccount = new Twitter(keys.configKeys)
 
+var twitterAccount = new Twitter({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+})
 
 router.use((req, res, next) => {
   res.header('access-control-allow-origin', '*')
   res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next();
+  next()
 })
 
-router.post("/", (req,res) => {
+router.post("/", (req, res) => {
   let tweets = []
 
   twitterAccount.get('search/tweets', {count: 100, q: '#ign', lang: 'en', exclude: "retweets"})
   .then((tweet) => {
-    let length = tweet.statuses.length
+    let tweetLength = tweet.statuses.tweetLength
 
-    for (var x = 0; x < length; x++) {
+    for (let x = 0; x < tweetLength; x++) {
       tweets.push({tweet: tweet.statuses[x].text})
     }
     res.send(tweets)
@@ -26,7 +30,7 @@ router.post("/", (req,res) => {
   .catch((err) => {
     console.log(err)
     throw error
-  });
+  })
 })
 
 module.exports = router
